9ccfe0982e55b80a021b1e1e69e20381d0d97934
Delio Vicini <delio.vicini@gmail.com>
2026-09-05 17:40:05 +0200

    Add <animation> scene description tag

--- src/sensors/tests/test_perspective.py
+++ src/sensors/tests/test_perspective.py
@@ -307,3 +307,25 @@ def test10_sensor_to_world_transform_parameter_update(variant_scalar_rgb):
     params['to_world'] = T().scale([2, 1, 1])
     with pytest.raises(RuntimeError, match="Scale factors in the camera-to-world transformation are not allowed!"):
         params.update()
+
+
+def test11_animation_xml(variant_scalar_rgb):
+    xml = """
+    <scene version="3.0.0">
+        <sensor type="perspective">
+            <animation name="to_world">
+                <transform time="0">
+                    <translate x="0" y="0" z="0"/>
+                </transform>
+                <transform time="1">
+                    <translate x="0" y="0" z="1"/>
+                </transform>
+            </animation>
+        </sensor>
+    </scene>
+    """
+    scene = mi.load_string(xml)
+    sensor = scene.sensors()[0]
+    ray, _ = sensor.sample_ray(0.5, 0, [0.5, 0.5], 0)
+    assert dr.allclose(ray.o, mi.Point3f(0, 0, 0.5), atol=0.01)
+
